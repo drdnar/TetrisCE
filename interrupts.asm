0da0e9e205e8d3c988d63deb71554725b3fa2810
Ander D'nar <drdnar@gmail.com>
2015-06-03 14:28:13 -0400

    That belongs in ti84pce.inc

--- interrupts.asm
+++ interrupts.asm
@@ -6,35 +6,6 @@
 ;  - Dispatch interrupts to correct handler
 
 
-pIntStatus	.equ	7000h
-mpIntStatus	.equ	0F00000h
-pIntMask	.equ	7004h
-mpIntMask	.equ	0F00004h
-pIntAck		.equ	7008h
-mpIntAck	.equ	0F00008h
-pIntLachEnable	.equ	700Ch
-mpIntLachEnable	.equ	0F0000Ch
-pIntXor		.equ	7010h
-mpIntXor	.equ	0F00010h
-pIntStatusMasked	.equ	7014h
-mpIntStatusMasked	.equ	0F00014h
-
-intOnKey	.equ	1
-intOnKeyB	.equ	0
-intTimer1	.equ	2
-intTimer1B	.equ	1
-intTimer2	.equ	4
-intTimer2B	.equ	2
-intTimer3	.equ	8
-intTimer3B	.equ	3
-intOsTimer	.equ	10h
-intOsTimerB	.equ	4
-intKbd		.equ	400h
-intKbdB		.equ	10
-intLcd		.equ	800h
-intLcdB		.equ	11
-intRtc		.equ	1000h
-intRtcB		.equ	12
 
 ;------ ResetInterrupts --------------------------------------------------------
 ResetInterrupts:
@@ -58,7 +29,16 @@ _:	.dw	intOnKey | intOsTimer | intRtc | 2000h, 0
 
 ;------ InitializeInterrupts ---------------------------------------------------
 InitializeInterrupts:
-
+; Initializes internal IM2.
+; Inputs:
+;  - None
+; Outputs:
+;  - Documented effect(s)
+; Destroys:
+;  - Assume all
+	di
+; Configure interrupt sources
+	
 
 
 ;------ InterruptHandler -------------------------------------------------------
--- tetris.asm
+++ tetris.asm
@@ -4,6 +4,77 @@
 
 	.org	userMem - 2
 	.db	tExtTok, tAsm84CeCmp
+
+	00 80 00 00
+19 30 00 00
+00 00 00 00
+19 00 08 00
+
+
+now it's
+00 80 00 00
+19 30 00 00
+00 00 00 00
+19 00 00 00
+
+00 00 00 00
+18 00 00 00
+
+
+
+Anyway. . . .
+USB not-plugged:
+00 80 08 00
+11 30 00 00
+00 00 00 00
+19 00 00 00
+
+00 00 00 00
+10 00 00 00
+
+
+	ld	hl, (0F00005)
+	res	5, (hl)
+	ret
+
+Crypto
+Inter-chip bus
+Watchdog timer
+USB
+USB DMA?
+
+
+Bit   	Interrupt Source   
+0 1	ON Button
+1 2	Timer 1
+2 4	Timer 2
+3 8	Timer 3
+4 10	Unknown, but disabling causes freeze (might be OS timer?)
+5 20	
+6 40	
+7 80	
+8 100	
+9 200	
+10 400	Keyboard
+11 800	LCD Controller
+12 1000	Real-Time Clock
+13 2000	???
+14 4000	
+15 8000	Unknown, but signal seems to be constantly on
+16 1 0000
+17 2 0000
+18 4 0000
+19 8 0000	???
+20 10 0000
+21 20 0000
+
+
+	
+	ld	hl, (0F00005h)
+	res	5, (hl)
+	ret
+
+	
 	
 	call	_RunIndicOff
 	
@@ -45,6 +116,15 @@ _:	ld	de, (hl)
 	
 	ld	(savedSp), sp
 	
+	di
+	
+	; Timers
+	
+	; Keyboard
+	call	ConfigureKeyboard
+	
+	; Interrupts
+	
 	; Text driver
 	call	SetTextMode
 	ld	hl, font
@@ -57,54 +137,38 @@ _:	ld	de, (hl)
 	ld	(lcdCol), hl
 	
 TitleScreen:
-	di
-;	call	ConfigureKeyboard
-	ld	hl, blarghsasdf
-	call	PutS
-_:	ld	a, (0F00020h)
-	and	1
-	jp	nz, Quit
-	call	KbdRawScan
+	ld	hl, 0F00000h
+	call	asdfasjdl
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	call	asdfasjdl
+	call	NewLine
+	
+	
+_:	call	KbdRawScan
 	or	a
 	jr	z, -_
-	call	DispByte
-	jr	-_
-	
-	jp	Quit
-blarghsasdf:
-	.db	"Thingy: ", 0
-	
-	
-	
-	ld	hl, 0F50000h
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	call	asdfasjdl
-	call	NewLine
-	
-	
-	call	GetKey
 	
 	jp	Quit
 
